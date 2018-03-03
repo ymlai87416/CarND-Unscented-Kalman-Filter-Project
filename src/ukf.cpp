a509f8778218167f69803dc8ae201284b27e65bb
ymlai87416 <ymlai87416@gmail.com>
2018-03-04 02:09:22 +0800

    fix ukf bug

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -147,8 +147,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   double dt = ( meas_package.timestamp_ - previous_timestamp_ )/1000000.0;
   previous_timestamp_ = meas_package.timestamp_;
 
-  if( dt > 0.0001 )
-    Prediction( dt );
+  Prediction( dt );
 
   if( meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_)
   {
